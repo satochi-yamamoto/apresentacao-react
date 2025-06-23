b7e8c9bc825d67fa79daf3aa13492e870753fcc6
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-23 15:07:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react-icons/fa']
+    }
   }
 });
