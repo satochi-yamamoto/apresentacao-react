ff0e9dffccd939bba35deb068aadb088cd2fa3f8
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-23 15:11:21 -0300

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// src/components/index.js
+export { default as Header } from './Header';
+export { default as Hero } from './Hero';
+export { default as About } from './About';
+export { default as Experience } from './Experience';
+export { default as Education } from './Education';
+export { default as Skills } from './Skills';
+export { default as Contact } from './Contact';
+export { default as Footer } from './Footer';
